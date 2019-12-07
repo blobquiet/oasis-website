ade17e76f80508b93312f7dd93edd429d2667578
David <soquiet@hotmail.com>
2019-12-06 20:44:02 -0600

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          My love I'm happy Hey!
+        </p>
+        <a
+          className="App-link"
+          href="https://youtube.com.co"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          No me toques
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
